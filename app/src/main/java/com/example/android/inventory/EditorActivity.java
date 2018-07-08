48c9da682cd841833443e870f44432244d005698
Alicia Pena <aliciapf3@yahoo.es>
2018-07-08 23:52:16 +0200

    create table

--- app/src/main/java/com/example/android/inventory/EditorActivity.java
+++ app/src/main/java/com/example/android/inventory/EditorActivity.java
@@ -164,7 +164,6 @@ public class EditorActivity extends AppCompatActivity {
                 return true;
             // Respond to a click on the "Delete" menu option
             case R.id.action_delete:
-                // Do nothing for now
                 return true;
             // Respond to a click on the "Up" arrow button in the app bar
             case android.R.id.home:
